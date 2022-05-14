54c2cccfcee58489322b5684763fa4756c07c78b
Rafael Lima | Zeero <rafaa99@gmail.com>
2022-05-13 22:33:49 -0300

    Mastering tuples

---
+++ tuples.ts
@@ -0,0 +1,10 @@
+const Hammr = {
+  level: 167,
+  class: 'mechanic',
+  crit: true
+}
+
+const rebornHammr = [175, 'mechanic', true]
+type CharType = [number, string, boolean]
+
+const newChar: CharType = [144, 'warlock', false]

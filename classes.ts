1eda80c25a953e3c765d4dde82a18127a7c3858c
Rafael Lima | Zeero <rafaa99@gmail.com>
2022-05-16 20:33:34 -0300

    Classes complete

--- classes.ts
+++ classes.ts
@@ -1,26 +1,57 @@
 class Warrior {
-  pierce(): void {
+  constructor(protected hpRegen: number = 5) {}
+
+  public getHpRegen(): void {
+    console.log(`Base hp regen: ${this.hpRegen}`)
+  }
+
+  public pierce(): void {
     console.log('Pierce ATK!')
   }
-  slash(): void {
+  public slash(): void {
     console.log('Sword Slash')
   }
 }
 
 class Swordsman extends Warrior {
-  swordplay(): void {
+  constructor(
+    public oneHandedSword: boolean,
+    public twoHandedSword: boolean,
+    hpRegenBonus?: number
+  ) {
+    super(hpRegenBonus ?? 10)
+  }
+  public swordplay(): void {
     console.log('Sword Play ATK!')
   }
-  slash(): void {
+  public slash(): void {
     console.log('Better sword slash!')
+  }
+  public hp() {
+    this.hpRegen += 10
+    console.log(`Extra hp regen: ${this.hpRegen}`)
   }
 }
 
-const herald = new Swordsman()
-const apprentice = new Warrior()
+const herald = new Swordsman(false, true, 13)
+const apprentice = new Warrior(17)
+const newbie = new Swordsman(true, false)
 
 herald.pierce()
 herald.swordplay()
 
 herald.slash()
 apprentice.slash()
+
+// modifiers:
+// Public-------> Anywhere, anytime
+// Private------> Other methods in this class
+// Protected----> extends Private to child classes
+
+herald.hp()
+
+apprentice.getHpRegen()
+
+newbie.getHpRegen()
+
+newbie.hp()
